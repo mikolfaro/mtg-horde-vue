f20b8dcab823ca5012d4c7918871ce7ad60961f1
Mikol Faro <mikol.faro@gmail.com>
2021-04-18 11:49:06 +0200

    Install vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,17 @@ module.exports = {
       }
     }
   },
+
   publicPath: process.env.NODE_ENV === 'production'
     ? '/mtg-horde-vue/'
-    : '/'
+    : '/',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
