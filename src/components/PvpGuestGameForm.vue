fd19bb99fd1f080a0d40026be27c8dcd017f7299
Mikol Faro <mikol.faro@gmail.com>
2021-04-21 09:32:59 +0200

    Stub Pvp Forms

---
+++ src/components/PvpGuestGameForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form v-on:submit="handleSubmit">
+    <input
+        class="button button-primary"
+        type="submit"
+        :value="$t('gameForm.submit')"
+    />
+  </form>
+</template>
+<script>
+export default {
+  name: "PvpGuestGameForm",
+  data() {
+    return {}
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      this.$emit("play", {
+
+      })
+    }
+  }
+}
+</script>
