eacce277b2aa499826e5defc56ea0acc1ee1f0dd
Mikol Faro <mikol.faro@gmail.com>
2021-04-21 14:05:37 +0200

    Pvp Host Game Form works

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import { firestorePlugin } from 'vuefire'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
 
 Vue.config.productionTip = false
+
+Vue.use(firestorePlugin)
 
 new Vue({
   router,
---
+++ src/views/PvpHostGame.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="game-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <div class="camera">
+            <span class="name">{{ playerName }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { auth } from '@/utils/firebase'
+
+export default {
+  name: "PvpHostGame",
+  data() {
+    return {
+      user: {},
+      playerName: null
+    }
+  },
+  mounted() {
+    auth.onAuthStateChanged((user) => {
+      this.user = user
+      this.playerName = user.displayName
+    })
+  },
+}
+</script>
+<style lang="sass" scoped>
+  .camera
+    min-width: 200px
+    min-height: 200px
+    background: red
+</style>
